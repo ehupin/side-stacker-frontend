400142658c029e4af56100b7a82a506906882b0d
ehupin <erick.hupin@gmail.com>
2021-02-16 18:38:58 -0500

    add 2 players mode

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 import React from 'react';
 import classNames from "classnames";
+
 const io = require("socket.io-client")
 
 import './App.css';
+
 const config = require('./config');
 
 // define game statuses and related messages
 const GameStatuses = Object.freeze({
   WaitingForConnection: {
-    alue: 0,
-    message: 'Wait for connection to server...'},
+    value: 0,
+    message: 'Wait for connection to server...'
+  },
   WaitingForOpponentToConnect: {
     value: 1,
-    message: 'Wait for opponent connection...'},
+    message: 'Wait for opponent connection...'
+  },
   WaitingForPlayerMove: {
     value: 2,
-    message: 'Play !'},
+    message: 'Play !'
+  },
   WaitingForOpponentMove: {
     value: 3,
-    message: 'Wait for opponent to play...'},
+    message: 'Wait for opponent to play...'
+  },
   PlayerWon: {
     value: 4,
-    message: 'You win :)'},
+    message: 'You win :)'
+  },
   OpponentWon: {
     value: 5,
-    message: 'You loose :('},
+    message: 'You loose :('
+  },
   Draw: {
     value: 6,
-    message: 'Draw :/'},
+    message: 'Draw :/'
+  },
 })
+
+const GameModes = {
+  OnePlayer: 0,
+  TwoPlayer: 1,
+}
 
 
 class App extends React.Component {
@@ -37,27 +51,28 @@ class App extends React.Component {
     const socket = io(config.serverUrl)
 
     this.state = {
+      gameMode: null,
       gameMatrix: [],
       socket,
-      gameId:null,
-      playerId:null,
+      gameId: null,
+      playerId: null,
       status: GameStatuses.WaitingForConnection.value
     };
 
     // wait for server connection and game to be joined
     const that = this
-    socket.on('connect', function() {
-      socket.on('player_joined_game', (data)=>{
+    socket.on('connect', function () {
+      socket.on('player_joined_game', (data) => {
 
         // if current player is joining the game (i.e. not player id has been defined yet)
-        if (that.state.playerId === null){
+        if (that.state.playerId === null) {
 
           // create a matrix made of "null"
           const gameMatrix = Array.from(
             {length: data.game_matrix_size},
             e => Array.from(
               {length: data.game_matrix_size},
-                e => null
+              e => null
             )
           );
 
@@ -70,7 +85,7 @@ class App extends React.Component {
 
         // update game status
         let status
-        if (data.game_is_full){
+        if (data.game_is_full) {
           status = that.state.playerId === 0 ? GameStatuses.WaitingForPlayerMove.value : GameStatuses.WaitingForOpponentMove.value
         } else {
           status = GameStatuses.WaitingForOpponentToConnect.value
@@ -80,7 +95,7 @@ class App extends React.Component {
     });
 
     // update component when a piece is added
-    socket.on('piece_added', (data)=>{
+    socket.on('piece_added', (data) => {
 
       // duplicate and update game matrix
       const editedGameTable = [...this.state.gameMatrix]
@@ -88,79 +103,140 @@ class App extends React.Component {
 
       // update game status
       let status
-      if (data.game_is_draw){
+      if (data.game_is_draw) {
         status = GameStatuses.Draw.value
-      } else if (data.game_is_won){
-        if (that.state.playerId === data.player_id){
+      } else if (data.game_is_won) {
+        if (that.state.playerId === data.player_id) {
           status = GameStatuses.PlayerWon.value
         } else {
           status = GameStatuses.OpponentWon.value
         }
       } else {
-        if (that.state.playerId === data.player_id){
+        if (that.state.playerId === data.player_id) {
           status = GameStatuses.WaitingForOpponentMove.value
         } else {
           status = GameStatuses.WaitingForPlayerMove.value
         }
       }
-      this.setState({gameMatrix:editedGameTable, status})
+      this.setState({gameMatrix: editedGameTable, status})
     })
   }
 
-  addButtonClicked(rowIndex, side){
+
+  joinGameClicked(gameMode) {
+    this.state.gameMode = gameMode
+    this.state.socket.emit('join', {gameMode})
+  }
+
+  render() {
+    return (
+      <div id="game">
+        <div id="header">
+          <div id="title">Side Stacker</div>
+          {this.state.gameMode === null
+            ? <StartupHeader joinFunction={this.joinGameClicked.bind(this)}/>
+            : <GameBoard parentState={this.state}/>
+          }
+
+        </div>
+
+
+      </div>
+    )
+  }
+
+}
+
+class StartupHeader extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <input type="button" className="joinButton" value="1 player"
+                 onClick={() => this.props.joinFunction(GameModes.OnePlayer)}/>
+          <input type="button" className="joinButton" value="2 player"
+                 onClick={() => this.props.joinFunction(GameModes.TwoPlayer)}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+class GameHeader extends React.Component {
+  render() {
+    return (
+      <div>
+        <div id="details">
+          <div>Game ID: {this.props.parentState.gameId}</div>
+        </div>
+        <div id="playerHeader">
+          <PlayerInfo playerId={0} currentPlayer={this.props.parentState.playerId}/>
+          <PlayerInfo playerId={1} currentPlayer={this.props.parentState.playerId}/>
+        </div>
+        <div id="info">
+          {this.props.statusMessage}
+        </div>
+      </div>
+    )
+  }
+}
+
+class GameBoard extends React.Component {
+  addButtonClicked(rowIndex, side) {
     // notify server for player move (if player is allowed to play)
-    if (this.state.status === GameStatuses.WaitingForPlayerMove.value){
-      this.state.socket.emit('add_piece', {rowIndex, side})
+    if (this.props.parentState.status === GameStatuses.WaitingForPlayerMove.value) {
+      this.props.parentState.socket.emit('add_piece', {rowIndex, side})
     }
   }
+
 
   render() {
     // list playable row in game matrix (i.e. one with no remaining null values)
     const playableRows = []
-    for (const row of this.state.gameMatrix){
+    for (const row of this.props.parentState.gameMatrix) {
       const playable = row.indexOf(null) !== -1
       playableRows.push(playable)
     }
 
     // get status message from game status
     const statusKey = Object.keys(GameStatuses).find((key) => {
-      return GameStatuses[key].value === this.state.status
+      return GameStatuses[key].value === this.props.parentState.status
     })
     const statusMessage = GameStatuses[statusKey].message
 
     // check if user can play
-    const canPlay = this.state.status === GameStatuses.WaitingForPlayerMove.value
+    const canPlay = this.props.parentState.status === GameStatuses.WaitingForPlayerMove.value
+
 
     return (
-      <div id="game">
-        <div id="header">
-          <div id="title">Side Stacker</div>
+      <div>
+        <div>
           <div id="details">
-            <div>Game ID: {this.state.gameId}</div>
+            <div>Game ID: {this.props.parentState.gameId}</div>
           </div>
           <div id="playerHeader">
-            <PlayerInfo playerId={0} currentPlayer={this.state.playerId}/>
-            <PlayerInfo playerId={1} currentPlayer={this.state.playerId}/>
+            <PlayerInfo playerId={0} currentPlayer={this.props.parentState.playerId}/>
+            <PlayerInfo playerId={1} currentPlayer={this.props.parentState.playerId}/>
           </div>
           <div id="info">
             {statusMessage}
           </div>
         </div>
         <div id="board">
-          {this.state.gameMatrix.map((row, rowIndex) => (
+          {this.props.parentState.gameMatrix.map((row, rowIndex) => (
             <div className='row' key={rowIndex}>
 
               <AddPieceButton side={0}
                               enabled={canPlay && playableRows[rowIndex]}
-                              clickHandler={()=>this.addButtonClicked(rowIndex, 0)}/>
+                              clickHandler={() => this.addButtonClicked(rowIndex, 0)}/>
 
               {row.map((column, columnIndex) => (
                 <div key={columnIndex}
                      className={
                        classNames({
                          cell: true,
-                         player0: this.state.gameMatrix[rowIndex][columnIndex] === 0,
-                         player1: this.state.gameMatrix[rowIndex][columnIndex] === 1,
+                         player0: this.props.parentState.gameMatrix[rowIndex][columnIndex] === 0,
+                         player1: this.props.parentState.gameMatrix[rowIndex][columnIndex] === 1,
                        })
                      }>
                 </div>
@@ -168,12 +244,12 @@ class App extends React.Component {
 
               <AddPieceButton side={1}
                               enabled={canPlay && playableRows[rowIndex]}
-                              clickHandler={()=>this.addButtonClicked(rowIndex, 1)}/>
+                              clickHandler={() => this.addButtonClicked(rowIndex, 1)}/>
             </div>
           ))}
         </div>
-
       </div>
+
     )
   }
 }
